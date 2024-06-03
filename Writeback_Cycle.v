f485b0bffa8e09f76979ea47f2441e73a86be540
levanduy093-work <levanduy.work@gmail.com>
2024-06-03 14:12:18 +0700

    Writeback Cycle

---
+++ Writeback_Cycle.v
@@ -0,0 +1,19 @@
+`include "Mux.v"
+
+module writeback_cycle(clk,rst,ResultSrcW,ALU_ResultW,ReadDataW,PCPlus4W,ResultW);
+
+    // Declaration input and output ports
+    input clk, rst, ResultSrcW;
+    input [31:0] ALU_ResultW, ReadDataW, PCPlus4W;
+
+    output [31:0] ResultW;
+
+    // Declaration of module
+    Mux result_mux (
+        .a(ALU_ResultW),
+        .b(ReadDataW),
+        .s(ResultSrcW),
+        .c(ResultW)
+    );
+
+endmodule
